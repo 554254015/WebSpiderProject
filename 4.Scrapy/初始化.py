c3b4df7321abe5bee4943f9e63e854ad40b578eb
554254015 <554254015@qq.com>
2018-12-18 23:24:32 +0800

    Update and rename 4.Scrapy抓取框架 to 4.Scrapy/初始化.py

---
+++ 4.Scrapy/初始化.py
@@ -0,0 +1,34 @@
+
+# Scrapy是Python开发的一个快速、高层次的屏幕抓取和Web抓取框架，用于抓取Web站点并从页面中提取结构化的数据。
+# Scrapy用途广泛，可以用于数据挖掘、监测和自动化测试。
+# Scrapy依赖于pywin32,Twisted,lxml,最后安装Scrapy
+
+import os 
+pname = input('项目名：')
+os.system("scrapy startproject " + pname)
+os.chdir(pname)
+wname = input('爬虫名：')
+sit = input('网址：')
+os.system('scrapy genspider ' + wname + ' ' + sit)
+runc = """
+from scrapy.srawler import CrawlerProcess
+from scrapy.utils.project import get_project_settings
+
+from %s.souders.%s import %s
+
+# 获取settings.py模块的设置
+settings = get_project_settings()
+process = CrawlerProcess(settings = settings)
+
+# 可以添加多个spider
+# process.crawl(Spider1)
+# process.crawl(Spider2)
+process.crawl(%s)
+
+# 启动爬虫，会阻塞，直到爬取完成
+process.start()
+""" % (pname, wname, wname[0].upper() + wname[1:] + 'Spider', wname[0].upper() + wname[1:] + 'Spider')
+
+with open('main.py', 'w', encoding = 'utf-8') as f:
+    f.write(runc)
+input('end')
