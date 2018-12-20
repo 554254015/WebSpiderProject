e6dce5425a86472db2cd945ad99a50db18d3c29a
554254015 <554254015@qq.com>
2018-12-20 11:07:41 +0800

    Update 初始化.py

--- 4.Scrapy/初始化.py
+++ 4.Scrapy/初始化.py
@@ -1,10 +1,4 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Tue Dec 18 22:58:32 2018
-
-@author: LINQUN
-"""
-
 # Scrapy是Python开发的一个快速、高层次的屏幕抓取和Web抓取框架，用于抓取Web站点并从页面中提取结构化的数据。
 # Scrapy用途广泛，可以用于数据挖掘、监测和自动化测试。
 # Scrapy依赖于pywin32,Twisted,lxml,最后安装Scrapy
